6915a1903acd16a284492cc836dd78a3a7de2f86
liang <823201523@qq.com>
2018-08-31 14:04:10 +0800

    0831-1400

--- cookbook/字典.py
+++ cookbook/字典.py
@@ -18,3 +18,12 @@ d.setdefault('a', []).append(2)
 d.setdefault('b', []).append(4)
 print(d)
 print(type(d))
+
+from collections import OrderedDict
+d = OrderedDict()
+d['foo'] = 1
+d['bar'] = 2
+d['spam'] = 3
+d['grok'] = 4
+print(d)
+
